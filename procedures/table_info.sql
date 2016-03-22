8718ee76f7384a14a95c5c023536b49738865329
c-pra <pr@xmarer.de>
2016-03-22 21:20:45 +0100

    *mod* remove spaces and tabs

--- procedures/table_info.sql
+++ procedures/table_info.sql
@@ -33,63 +33,63 @@ DROP PROCEDURE IF EXISTS `get_table_info`$$
 CREATE PROCEDURE `get_table_info` (
 	IN dbname VARCHAR(64))
 BEGIN
-	DECLARE EXIT HANDLER FOR SQLEXCEPTION 
-    BEGIN 
+	DECLARE EXIT HANDLER FOR SQLEXCEPTION
+	BEGIN
 		-- Check passed data base name
 		IF ((dbname IS NULL) OR (LENGTH(dbname) NOT BETWEEN 1 AND 64)) THEN
 			SIGNAL SQLSTATE '45000'
 				SET MESSAGE_TEXT = 'An invalid parameter was passed to the stored procedure.';
-		END IF; 
-		
+		END IF;
+
 		-- Check if the data base exists
 		if (SELECT COUNT(*) FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = dbname) = 0 then
 			SIGNAL SQLSTATE '45000'
-				SET MESSAGE_TEXT = 'The data base was not found.';	
+				SET MESSAGE_TEXT = 'The data base was not found.';
 		END IF;
 	END;
-    
-    SET @tmptbl = "cmysql_lib_get_table_info_tmptbl";
-    
+
+	SET @tmptbl = "cmysql_lib_get_table_info_tmptbl";
+
 	-- Load the results into a temporary table
 	CALL load_table_info(dbname, @tmptbl);
-		    
-    -- Get table names and row count values from the temp table
-	SET @sql_seltmptbl = CONCAT('SELECT `Name`, `Rows` FROM ', @tmptbl, ' ORDER BY `name`;'); 
+
+	-- Get table names and row count values from the temp table
+	SET @sql_seltmptbl = CONCAT('SELECT `Name`, `Rows` FROM ', @tmptbl, ' ORDER BY `name`;');
 	-- Execute the statement
-	PREPARE            stmt_seltmptbl FROM @sql_seltmptbl;                                                                                                                                               
-	EXECUTE            stmt_seltmptbl;
-	DEALLOCATE PREPARE stmt_seltmptbl;
-    
-   	-- Drops the temporary table 
+	PREPARE				stmt_seltmptbl FROM @sql_seltmptbl;
+	EXECUTE				stmt_seltmptbl;
+	DEALLOCATE PREPARE	stmt_seltmptbl;
+
+	-- Drops the temporary table
 	SET @sql_drptmptbl = CONCAT('DROP TEMPORARY TABLE IF EXISTS ', @tmptbl, ';');
 	-- Execute the statement
-	PREPARE            stmt_drptmptbl FROM @sql_drptmptbl;                                                                                                                                               
-	EXECUTE            stmt_drptmptbl;            
-	DEALLOCATE PREPARE stmt_drptmptbl;
+	PREPARE				stmt_drptmptbl FROM @sql_drptmptbl;
+	EXECUTE				stmt_drptmptbl;
+	DEALLOCATE PREPARE	stmt_drptmptbl;
 END$$ 
- 
+
 DROP PROCEDURE IF EXISTS `load_table_info`$$
 CREATE PROCEDURE `load_table_info` (
-	IN dbname     CHAR(64), 
-	IN tmptbl CHAR(64))
+	IN dbname	CHAR(64),
+	IN tmptbl	CHAR(64))
 BEGIN
 	DECLARE tblname CHAR(64);
-	DECLARE eoquery INT DEFAULT FALSE;  
+	DECLARE eoquery INT DEFAULT FALSE;
 
 	-- Get all tables from the data base
-    DECLARE curInformationSchema CURSOR FOR                                                                                                                                                  
+	DECLARE curInformationSchema CURSOR FOR
 		SELECT 
-			tbl.table_name                                                                                                                                                 
+			tbl.table_name
 		FROM 
-			information_schema.tables tbl                                                                                                                                    
+			information_schema.tables tbl
 		WHERE 
 			tbl.table_schema = dbname AND tbl.table_type = 'BASE TABLE';
-            
+
 	DECLARE CONTINUE HANDLER FOR NOT FOUND SET eoquery = TRUE;
-    
-    -- Error handling
-    DECLARE EXIT HANDLER FOR SQLEXCEPTION 
-    BEGIN 
+
+	-- Error handling
+	DECLARE EXIT HANDLER FOR SQLEXCEPTION 
+	BEGIN 
 		-- Check the passed data base name
 		IF ((dbname IS NULL) OR (LENGTH(dbname) NOT BETWEEN 1 AND 64)) THEN
 			SIGNAL SQLSTATE '45000'
@@ -99,54 +99,54 @@ BEGIN
 		-- Check whether the data base exists
 		if (SELECT COUNT(*) FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = dbname) = 0 then
 			SIGNAL SQLSTATE '45000'
-				SET MESSAGE_TEXT = 'The database was not found.';	
-		END IF;    
-        
+				SET MESSAGE_TEXT = 'The database was not found.';
+		END IF;
+
 		-- Check the passed temporary table name
 		IF ((tmptbl IS NULL) OR (LENGTH(tmptbl) NOT BETWEEN 1 AND 64)) THEN
 			SIGNAL SQLSTATE '45000'
 				SET MESSAGE_TEXT = 'Invalid parameter specified for temporary table name.';
-		END IF;    
-    END;
-   
-    -- Drops the temporary table before we start
-    SET @sql_drptmptbl = CONCAT('DROP TEMPORARY TABLE IF EXISTS ', tmptbl, ';');
-    -- Execute the statement
-	PREPARE            stmt_drptmptbl FROM @sql_drptmptbl;                                                                                                                                               
-	EXECUTE            stmt_drptmptbl;            
-	DEALLOCATE PREPARE stmt_drptmptbl;
-    
-    -- Create the new temp table where the result set is inserted.
-	SET @sql_crttmptbl = CONCAT('CREATE TEMPORARY TABLE ', tmptbl,   ' ', 
-					            '(                                     ', 
-						        '   `Name` VARCHAR(64) PRIMARY KEY,    ',
-                                '   `Rows` INT                         ',
-                                ');                                    ');
-	
-	PREPARE            stmt_crttmptbl FROM @sql_crttmptbl;                                                                                                                                               
-	EXECUTE            stmt_crttmptbl;
-	DEALLOCATE PREPARE stmt_crttmptbl;
-        
+		END IF;
+	END;
+
+	-- Drops the temporary table before we start
+	SET @sql_drptmptbl = CONCAT('DROP TEMPORARY TABLE IF EXISTS ', tmptbl, ';');
+	-- Execute the statement
+	PREPARE				stmt_drptmptbl FROM @sql_drptmptbl;
+	EXECUTE				stmt_drptmptbl;
+	DEALLOCATE PREPARE	stmt_drptmptbl;
+
+	-- Create the new temp table where the result set is inserted.
+	SET @sql_crttmptbl = CONCAT('CREATE TEMPORARY TABLE ', tmptbl,	'	',
+								'(										',
+								'	`Name` VARCHAR(64) PRIMARY KEY,		',
+								'	`Rows` INT							',
+								');										');
+
+	PREPARE				stmt_crttmptbl FROM @sql_crttmptbl;
+	EXECUTE				stmt_crttmptbl;
+	DEALLOCATE PREPARE	stmt_crttmptbl;
+
 	OPEN curInformationSchema;
 	-- Open the cursor and iterate through tables of the schema. Query the row count 
-    -- for each table and insert the value into the temporary table
-    query_loop: LOOP                                                                                                                                                       
-		FETCH curInformationSchema INTO tblname;                                                                                                                                          
+	-- for each table and insert the value into the temporary table
+	query_loop: LOOP
+		FETCH curInformationSchema INTO tblname;
 
 		-- Check end of result set
-		IF eoquery THEN                                                                                                                                           
-			LEAVE query_loop;                                                                                                                                              
-		END IF;                                                                                                                                                             
+		IF eoquery THEN
+			LEAVE query_loop;
+		END IF;
 
 		-- Query row count and insert the value into the temporary table
 		SET @sql_instmptbl = CONCAT('INSERT INTO ', tmptbl, ' (name, rows) SELECT ''', tblname, ''', COUNT(*) AS Count FROM ' , dbname, '.', tblname);
-        -- Execute statement
+		-- Execute statement
 		PREPARE            stmt_instmptbl FROM @sql_instmptbl;
 		EXECUTE            stmt_instmptbl;
 		DEALLOCATE PREPARE stmt_instmptbl;
-	END LOOP;                                                                                                                                                               
+	END LOOP;
 
-	CLOSE curInformationSchema;           
+	CLOSE curInformationSchema;
 END$$
 
 DELIMITER ;
